20c03b41dd74a888655dbc2e50d604979e002627
Gabriel Caires <gbrlcaires@gmail.com>
2022-06-29 18:19:59 -0300

    projeto finalizado

--- app/src/main/java/com/devmasterteam/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/devmasterteam/gastoviagem/MainActivity.kt
@@ -24,7 +24,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun isValid(): Boolean {
+        return (!binding.editDistance.text.toString().isNullOrEmpty()
+                && !binding.editPrice.text.toString().isNullOrEmpty()
+                && !binding.editAutonomy.text.toString().isNullOrEmpty()
+                && binding.editAutonomy.text.toString().toFloat() > 0f)
+    }
+
     fun calculate() {
-        Toast.makeText(this, "Calculando....", Toast.LENGTH_SHORT).show()
+
+        if (isValid()) {
+            var distance = binding.editDistance.text.toString().toFloat()
+
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
     }
 }
